70eb52dc5b91ba726d0f0bf84ad6d0b544b18f54
Stas <stasovskii@gmail.com>
2022-11-27 08:31:39 +0200

    ucheba probnoe sohranenie

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm("ты тута?");
+// console.log(result);
+
+// const answer = prompt("есть 18?", "");
+// console.log(typeof(answer));
+
+const answers = [];
+answers[0] = prompt('как зовут?', '');
+answers[1] = prompt('фамилия?', '');
+answers[2] = prompt('сколько лет?', '');
+
+document.write(answers);
