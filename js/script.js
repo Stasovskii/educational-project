a660df3f6eed992ce5ec4f5a08be6bc271f09ea0
stas <stasovskii@gmail.com>
2022-12-01 13:28:35 +0200

    Domashnee zadanie

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 "use strict";
 
-// alert('hello');
+alert('hello');
 
-// const result = confirm("ты тута?");
-// console.log(result);
+const result = confirm("ты тута?");
+console.log(result);
 
-// const answer = prompt("есть 18?", "");
-// console.log(typeof(answer));
+const answer = prompt("есть 18?", "");
+console.log(typeof(answer));
 
 const answers = [];
 answers[0] = prompt('как зовут?', '');
@@ -14,3 +14,24 @@ answers[1] = prompt('фамилия?', '');
 answers[2] = prompt('сколько лет?', '');
 
 document.write(answers);
+
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', ''),
+          c = prompt('Один из последних просмотренных фильмов?', ''),
+          d = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
+
